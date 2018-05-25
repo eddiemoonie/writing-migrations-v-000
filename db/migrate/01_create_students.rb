52417ca244de7da6639be8162520a92a40e6e7f6
Eddie Moon <eddiemoonie@gmail.com>
2018-05-25 19:25:24 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -3,6 +3,8 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |s|
       s.string :name
+      s.integer :grade
+      s.string :date
     end
   end
 
